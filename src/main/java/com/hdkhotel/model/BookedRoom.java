615b3a60b5f6425ef20b2ead59b5f43c6eacf5c0
NKDuy <nkduy.dev@gmail.com>
2024-05-12 16:56:18 +0700

    chore: update Room + BookedRoom

--- src/main/java/com/hdkhotel/model/BookedRoom.java
+++ src/main/java/com/hdkhotel/model/BookedRoom.java
@@ -1,6 +1,6 @@
 package com.hdkhotel.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,15 +14,36 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookedRoom {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long bookingId;
+
+  @Column(name = "check_In")
   private LocalDate checkInDate;
+
+  @Column(name = "check_Out")
   private LocalDate checkOutDate;
+
+  @Column(name = "guest_FullName")
   private String guestFullName;
+
+  @Column(name = "guest_Email")
   private String guestEmail;
+
+  @Column(name = "adults")
   private int NumOfAdults;
+
+  @Column(name = "children")
   private int NumOfChildren;
+
+  @Column(name = "total_guest")
   private int totalNumOfGuest;
+
+  @Column(name = "confirmation_Code")
   private String bookingConfirmationCode;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "room_id")
   private Room room;
 
   public void calculateTotalNumberOfGuest() {
