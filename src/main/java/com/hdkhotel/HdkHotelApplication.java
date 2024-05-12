a181d0231fc923f0eb30a2a206e6f472c92ee3c6
NKDuy <nkduy.dev@gmail.com>
2024-05-12 14:44:01 +0700

    chor: init

---
+++ src/main/java/com/hdkhotel/HdkHotelApplication.java
@@ -0,0 +1,13 @@
+package com.hdkhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdkHotelApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HdkHotelApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hdkhotel/HdkHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdkhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdkHotelApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
