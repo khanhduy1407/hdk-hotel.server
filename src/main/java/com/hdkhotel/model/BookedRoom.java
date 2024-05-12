5580558857ece05499a6ab0bcd642df7d9c1443e
NKDuy <nkduy.dev@gmail.com>
2024-05-12 16:32:19 +0700

    chore: update

--- src/main/java/com/hdkhotel/model/BookedRoom.java
+++ src/main/java/com/hdkhotel/model/BookedRoom.java
@@ -1,7 +1,18 @@
 package com.hdkhotel.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
   private Long bookingId;
   private LocalDate checkInDate;
@@ -11,4 +22,24 @@ public class BookedRoom {
   private int NumOfAdults;
   private int NumOfChildren;
   private int totalNumOfGuest;
+  private String bookingConfirmationCode;
+  private Room room;
+
+  public void calculateTotalNumberOfGuest() {
+    this.totalNumOfGuest = this.NumOfAdults + this.NumOfChildren;
+  }
+
+  public void setNumOfAdults(int numOfAdults) {
+    NumOfAdults = numOfAdults;
+    calculateTotalNumberOfGuest();
+  }
+
+  public void setNumOfChildren(int numOfChildren) {
+    NumOfChildren = numOfChildren;
+    calculateTotalNumberOfGuest();
+  }
+
+  public void setBookingConfirmationCode(String bookingConfirmationCode) {
+    this.bookingConfirmationCode = bookingConfirmationCode;
+  }
 }
