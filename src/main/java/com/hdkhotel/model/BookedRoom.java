09fa3fb50873291f569eb4b61ea1a82f5f62b178
NKDuy <nkduy.dev@gmail.com>
2024-05-12 16:26:06 +0700

    chore: update

--- src/main/java/com/hdkhotel/model/BookedRoom.java
+++ src/main/java/com/hdkhotel/model/BookedRoom.java
@@ -1,4 +1,14 @@
 package com.hdkhotel.model;
 
+import java.time.LocalDate;
+
 public class BookedRoom {
+  private Long bookingId;
+  private LocalDate checkInDate;
+  private LocalDate checkOutDate;
+  private String guestFullName;
+  private String guestEmail;
+  private int NumOfAdults;
+  private int NumOfChildren;
+  private int totalNumOfGuest;
 }
