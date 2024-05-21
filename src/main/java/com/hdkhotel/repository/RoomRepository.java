580db31091b7fb21db3e18b660ebd35b9a47b18a
NKDuy <nkduy.dev@gmail.com>
2024-05-21 21:46:29 +0700

    chore: add get available rooms api

--- src/main/java/com/hdkhotel/repository/RoomRepository.java
+++ src/main/java/com/hdkhotel/repository/RoomRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -12,4 +13,12 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
 
   @Query("select distinct r.roomType from Room r")
   List<String> findDistinctRoomTypes();
+
+  @Query("select r from Room r " +
+         "where r.roomType like %:roomType%" +
+         "and r.id not in (" +
+         "  select br.room.id from BookedRoom br " +
+         "  where ((br.checkInDate <= :checkOutDate) and (br.checkOutDate >= :checkInDate))" +
+         ")")
+  List<Room> findAvailableRoomsByDatesAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
 }
