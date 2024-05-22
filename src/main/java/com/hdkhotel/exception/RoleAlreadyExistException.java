ebb3edf59ccf34b2dd314cbc617e27dacb58f839
NKDuy <nkduy.dev@gmail.com>
2024-05-23 00:24:12 +0700

    feat: add role controller

---
+++ src/main/java/com/hdkhotel/exception/RoleAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.hdkhotel.exception;
+
+public class RoleAlreadyExistException extends RuntimeException {
+  public RoleAlreadyExistException(String message) {
+    super(message);
+  }
+}
