fcd332aea954a02a73e9d07e4a93ad2eae57976c
NKDuy <nkduy.dev@gmail.com>
2024-05-24 09:43:02 +0700

    fix: remove FilterRegistrationBean in cors config

--- src/main/java/com/hdkhotel/security/CorsConfig.java
+++ src/main/java/com/hdkhotel/security/CorsConfig.java
@@ -18,25 +18,38 @@ public class CorsConfig {
   private static final Long MAX_AGE = 3600L;
   private static final int CORS_FILTER_ORDER = -102;
 
+//  @Bean
+//  public FilterRegistrationBean<CorsFilter> corsFilter() {
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    CorsConfiguration config = new CorsConfiguration();
+//    config.setAllowCredentials(true);
+//    config.addAllowedOrigin("*");
+//    config.setAllowedHeaders(Arrays.asList(
+//      HttpHeaders.AUTHORIZATION,
+//      HttpHeaders.CONTENT_TYPE,
+//      HttpHeaders.ACCEPT));
+//    config.setAllowedMethods(Arrays.asList(
+//      HttpMethod.GET.name(),
+//      HttpMethod.POST.name(),
+//      HttpMethod.PUT.name(),
+//      HttpMethod.DELETE.name()));
+//    config.setMaxAge(MAX_AGE);
+//    source.registerCorsConfiguration("/**", config);
+//    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+//    bean.setOrder(CORS_FILTER_ORDER);
+//    return bean;
+//  }
+
   @Bean
-  public FilterRegistrationBean corsFilter() {
+  public CorsFilter corsFilter() {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true);
     config.addAllowedOrigin("*");
-    config.setAllowedHeaders(Arrays.asList(
-      HttpHeaders.AUTHORIZATION,
-      HttpHeaders.CONTENT_TYPE,
-      HttpHeaders.ACCEPT));
-    config.setAllowedMethods(Arrays.asList(
-      HttpMethod.GET.name(),
-      HttpMethod.POST.name(),
-      HttpMethod.PUT.name(),
-      HttpMethod.DELETE.name()));
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
     config.setMaxAge(MAX_AGE);
     source.registerCorsConfiguration("/**", config);
-    FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-    bean.setOrder(CORS_FILTER_ORDER);
-    return bean;
+    return new CorsFilter(source);
   }
 }
