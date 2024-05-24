d4d415af67318f2b17c786bc4e56d341f8458105
NKDuy <nkduy.dev@gmail.com>
2024-05-24 09:56:37 +0700

    chore: add request body

--- src/main/java/com/hdkhotel/service/UserService.java
+++ src/main/java/com/hdkhotel/service/UserService.java
@@ -28,6 +28,7 @@ public class UserService implements IUserService {
     }
 
     user.setPassword(passwordEncoder.encode(user.getPassword()));
+    System.out.println(user.getPassword());
     Role userRole = roleRepository.findByName("ROLE_USER").get();
     user.setRoles(Collections.singletonList(userRole));
 
